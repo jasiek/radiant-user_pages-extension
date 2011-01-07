fa3bd2eb0d808f516e54c06f3fa152abdee7ad04
Jan Szumiec <jan.szumiec@gmail.com>
2011-01-07 12:08:06 -0500

    Minor refactoring, all tests are passing.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  # FIXME: workaround for tests
+  attr_accessor :created_by
+
   def can?(action, page)
     UserPagePermission.count(:conditions => ['user_id = ? AND page_id = ? AND action = ?', self.id, page.id, action.to_s]) > 0
   end
--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -4,27 +4,52 @@ def random_string
   Digest::SHA1.hexdigest(Kernel.rand.to_s)
 end
 
+def default_status
+  Status.find(1)
+end
+
+def default_page_params(additional_params={})
+  {
+    :title => random_string(),
+    :slug => random_string(),
+    :breadcrumb => random_string(),
+    :status => default_status()
+  }.merge(additional_params)
+end
+
+def with_disabled_observers(&blk)
+  Page.delete_observers
+  UserPagePermission.delete_observers
+  blk.call
+  PageObserver.instance.send(:add_observer!, Page)
+  UserPagePermissionObserver.instance.send(:add_observer!, UserPagePermission)
+end
+
 describe Page do
   before :all do
     @user = User.create!
+    with_disabled_observers do
+      @page_with_permission = Page.create!(default_page_params())
+      @page_without_permission = Page.create!(default_page_params())
+    end
     PageObserver.current_user = @user
     UserPagePermissionObserver.current_user = @user
-    @page_with_permission = Page.create!(:title => random_string(), :slug => random_string(), :breadcrumb => random_string(), :status => Status.find(1))
-    @page_without_permission = Page.create!(:title => random_string(), :slug => random_string(), :breadcrumb => random_string(), :status => Status.find(1))
   end
   
   describe 'creation' do
     before :each do
-      @user_page_permission = UserPagePermission.create!(:user_id => @user, :page_id => @page_with_permission, :action => 'create')
+      with_disabled_observers do
+        @user_page_permission = UserPagePermission.create!(:user_id => @user.id, :page_id => @page_with_permission.id, :action => 'create')
+      end
     end
 
     it "should allow the user to create a new page" do
-      Page.create!(:parent => @page_with_permission, :title => random_string(), :slug => random_string(), :breadcrumb => random_string(), :status => Status.find(1))
+      Page.create!(default_page_params(:parent => @page_with_permission))
     end
 
     it "should prevent the user from creating a new page" do
       lambda do
-        Page.create!(:parent => @page_without_permission, :title => random_string(), :slug => random_string(), :breadcrumb => random_string(), :status => Status.find(1))
+        Page.create!(default_page_params(:parent => @page_without_permission))
       end.should raise_exception(UserPagesExtension::AccessDenied)
     end
 
@@ -35,7 +60,9 @@ describe Page do
 
   describe 'removal' do
     before :each do
-      @user_page_permission = UserPagePermission.create!(:user_id => @user, :page_id => @page_with_permission, :action => 'destroy')
+      with_disabled_observers do
+        @user_page_permission = UserPagePermission.create!(:user_id => @user.id, :page_id => @page_with_permission.id, :action => 'destroy')
+      end
     end
 
     it "should allow the user to destroy the page" do
@@ -55,7 +82,9 @@ describe Page do
   
   describe 'modification' do
     before :each do
-      @user_page_permission = UserPagePermission.create!(:user_id => @user, :page_id => @page_with_permission, :action => 'update')
+      with_disabled_observers do
+        @user_page_permission = UserPagePermission.create!(:user_id => @user.id, :page_id => @page_with_permission.id, :action => 'update')
+      end
     end
 
     it "should allow the user to edit the page" do
@@ -77,23 +106,40 @@ describe Page do
 
   describe 'permission modification' do
     before :each do
-      @user_page_permission = UserPagePermission.create!(:user_id => @user, :page_id => @page_with_permission, :action => 'permissions')
+      with_disabled_observers do
+        @user_page_permission = UserPagePermission.create!(:user_id => @user.id, :page_id => @page_with_permission.id, :action => 'permissions')
+      end
     end
 
     it "should allow the user to change permissions" do
       @user_with_permission = @user
-      @page_with_permission.permissions.create!(:user_id => @user_with_permission, :action => 'create')
+      @page_with_permission.permissions.create!(:user_id => @user_with_permission.id, :action => 'create')
     end
 
     it "should prevent the user from changing permissions" do
       @user_without_permission = @user
       lambda do
-        @page_without_permission.permissions.create!(:user_id => @user_without_permission, :action => 'create')
+        @page_without_permission.permissions.create!(:user_id => @user_without_permission.id, :action => 'create')
       end.should raise_exception(UserPagesExtension::AccessDenied)
     end
 
     after :each do
       @user_page_permission.destroy
+    end
+  end
+
+  describe 'permission propagation on creation' do
+    before :each do
+      with_disabled_observers do
+        ['create', 'destroy', 'update'].each do |action|
+          UserPagePermission.create!(:user_id => @user.id, :page_id => @page_with_permission.id, :action => action)
+        end
+      end
+    end
+
+    it 'should copy permissions from parent to child when a new page is created' do
+      @new_page = Page.create!(default_page_params(:parent => @page_with_permission))
+      Set[@new_page.permissions.map(&:action)].should == Set[@page_with_permission.permissions.map(&:action)]
     end
   end
 

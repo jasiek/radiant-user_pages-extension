2fa01a0994ea62a99b05b27abefe7efa7c7539b9
Jan Szumiec <jan.szumiec@gmail.com>
2011-01-12 21:08:31 +0100

    Permissions are persisted when a new page is created.

--- lib/admin_pages_controller_extension.rb
+++ lib/admin_pages_controller_extension.rb
@@ -1,14 +1,15 @@
 module AdminPagesControllerExtension
   def self.included(base)
     base.before_filter :attach_css
-    base.around_filter :persist_permissions, :only => :update
+    base.around_filter :persist_permissions_on_update, :only => :update
+    base.around_filter :persist_permissions_on_create, :only => :create
   end
 
   def attach_css
     include_stylesheet 'admin/user_page'
   end
 
-  def persist_permissions(&blk)
+  def persist_permissions_on_update(&blk)
     UserPagePermission.transaction do
       @page.permissions.clear
       params[:page][:permissions].each do |query_string|
@@ -18,6 +19,18 @@ module AdminPagesControllerExtension
       end
       params[:page].delete(:permissions)
       yield
+    end
+  end
+
+  def persist_permissions_on_create(&blk)
+    UserPagePermission.transaction do
+      permissions = params[:page].delete(:permissions)
+      yield
+      permissions.each do |query_string|
+        next if query_string.empty?
+        user_id, action = query_string.split(/,/)
+        @page.permissions.create!(:user => User.find(user_id), :action => action)
+      end
     end
   end
 end
